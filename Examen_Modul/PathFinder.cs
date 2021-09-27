c95814c615c0fefa17cab89b5a912aeeaebf3ab8
Funkyman3438@mail.ru <Funkyman3438@mail.ru>
2021-09-27 16:47:00 +0300

    Работа над созданием списка всех путей

--- Examen_Modul/PathFinder.cs
+++ Examen_Modul/PathFinder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Examen_Modul
 {
     class PathFinder
     {
+        List<Activity> activities = new List<Activity>(); //Список всех работ (в графике это дуги)
+        List<Path> pathes = new List<Path>(); //Список всех путей
 
     }
 }
