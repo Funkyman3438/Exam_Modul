e5b607473c6f8bb95e0fb8f8ede75ac201de39d5
Funkyman3438@mail.ru <Funkyman3438@mail.ru>
2021-09-27 16:47:44 +0300

    Создание методов для поиска начальной и конечной точки

--- Examen_Modul/PathFinder.cs
+++ Examen_Modul/PathFinder.cs
@@ -11,6 +11,19 @@ namespace Examen_Modul
     {
         List<Activity> activities = new List<Activity>(); //Список всех работ (в графике это дуги)
         List<Path> pathes = new List<Path>(); //Список всех путей
+        int FindStartingPos() //Метод для поиска начальной точки
+        {
+            foreach (Activity activity in activities) //Если нет таких дуг, которые бы входили в данную точку, то она начальная.
+                if (activities.Where(x => x.eventEnd == activity.eventStart).Count() == 0) return activity.eventStart;
+            return -1;
+        }
+
+        int FindEndingPos() //Метод для поиска конечной точки
+        {
+            foreach (Activity activity in activities) //Если нет таких дуг, которые бы исходили из данной точки, то она конечная.
+                if (activities.Where(x => x.eventStart == activity.eventEnd).Count() == 0) return activity.eventEnd;
+            return -1;
+        }
 
     }
 }
